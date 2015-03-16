166f1ba6466bd487dca715f54d0afbd5965914f7
Kaleb J. Barker <kaleb@brainfork.net>
2015-03-15 22:44:57 -0400

    Adding portfolio post type

---
+++ post-types/portfolio/admin.php
@@ -0,0 +1,23 @@
+<?php
+
+function add_new_portfolio_project_columns( $portfolio_project_columns ) {
+	$new_columns['id'] = __('ID');  
+	
+	foreach ($new_columns as $key => $value) {
+		$portfolio_project_columns[$key] = $value;
+	}
+	
+	return $portfolio_project_columns;
+}
+add_filter('manage_edit-portfolio_project_columns', 'add_new_portfolio_project_columns');
+
+function manage_portfolio_project_columns( $column_name, $id ) {
+	switch ( $column_name ) {
+		case 'id':
+			echo $id;
+		break;
+	}
+}
+add_action('manage_portfolio_project_posts_custom_column', 'manage_portfolio_project_columns', 10, 2);
+
+?>
---
+++ post-types/portfolio/meta.php
@@ -0,0 +1,15 @@
+<?php
+
+if ( ! function_exists( 'bf_cpt_portfolio_register_meta' ) ) {
+	function bf_cpt_portfolio_register_meta() {
+
+		/* Register post meta (note: you might want to alter the callbacks). */
+
+		register_meta( 'post', '_project_url',        'esc_url_raw', '__return_true' );
+		register_meta( 'post', '_project_client',     'strip_tags',  '__return_true' );
+		register_meta( 'post', '_project_location',   'strip_tags',  '__return_true' );
+		register_meta( 'post', '_project_start_date', 'strip_tags',  '__return_true' );
+		register_meta( 'post', '_project_end_date',   'strip_tags',  '__return_true' );
+	}
+	add_action( 'init', 'bf_cpt_portfolio_register_meta' );
+}
